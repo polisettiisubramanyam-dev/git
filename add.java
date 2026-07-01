523aa22aaa6755f37f619f6026ce22b8d259d983
polisettiisubramanyam-dev <subramanyam.polisetti@sasi.ac.in>
2026-07-01 11:46:43 +0530

    The is the my second commit

---
+++ add.java
@@ -0,0 +1,12 @@
+public class add {
+    public static void add(int a, int b) {
+        System.out.println("The addition: ", a+b);
+    }
+    public static void sub(int a, int b) {
+        System.out.println("The subraction: ", a-b);
+    }
+    
+    public static void main(String[] args) {
+        add(10,20);
+    }
+}
